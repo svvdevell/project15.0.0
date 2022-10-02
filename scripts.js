8cbe39b2dc673c9251364e5fc9c2f7d663087131
svvdevell <svvdevel@gmail.com>
2022-10-02 14:10:39 +0300

    added task

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 "use strict"
 
+let albumList = document.getElementById("albums");
+albumList.style.cssText = "color: blue; list-style: none; padding: 0";
+
+fetch("https://jsonplaceholder.typicode.com/albums")
+    .then((response) => response.json())
+    .then((result) => {
+        result.forEach((element) => {
+            let albumTitle = document.createElement("li");
+            albumTitle.innerHTML = element.title;
+            albumTitle.classList.add("album_title");
+            albumList.append(albumTitle);
+        });
+    })
+    .catch((error) => console.error(error));
